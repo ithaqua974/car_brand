11f1b53a532e3172403f8e1e864c65e316d1ed72
ithaqua974 <tristan.hoarau@gmail.com>
2020-04-03 17:47:12 +0400

    fixe

--- Routes/indexRoute.js
+++ Routes/indexRoute.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const carController = require('../controllers/carController');
 
 router.get('/', carController.list1);
-router.get('/', carController.list2);
+router.get('/model', carController.list2);
 // router.get('/getinfo/:id', carController.getinfo);
 
 
